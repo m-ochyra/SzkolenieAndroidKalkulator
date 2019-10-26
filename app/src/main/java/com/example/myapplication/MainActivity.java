8a15adba1f3bfcab80a1f55f42e2eeaa19b9a5b4
Marcin Ochyra <marcinochyra@MacBook-Pro-Marcin.local>
2019-10-26 12:43:48 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText etNumberOne;
+    EditText etNumberTwo;
+    Button bAdd;
+    Button bSub;
+    Button bMultiply;
+    Button bDivide;
+    TextView tvResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etNumberOne = findViewById(R.id.etNumberOne);
+        etNumberTwo = findViewById(R.id.etNumberTwo);
+        bAdd = findViewById(R.id.bAdd);
+        bSub = findViewById(R.id.bSub);
+        bMultiply = findViewById(R.id.bMultiply);
+        bDivide = findViewById(R.id.bDivide);
+        tvResult = findViewById(R.id.tvResult);
+
+        bAdd.setOnClickListener(this);
+        bSub.setOnClickListener(this);
+        bMultiply.setOnClickListener(this);
+        bDivide.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String value1 = etNumberOne.getText().toString();
+        String value2 = etNumberTwo.getText().toString();
+
+        if(value1.isEmpty() || value2.isEmpty()) {
+            Toast.makeText(this, "Nie podano wartości", Toast.LENGTH_SHORT).show();
+        } else {
+            int number1 = Integer.valueOf(value1);
+            int number2 = Integer.valueOf(value2);
+
+            switch (v.getId()) {
+                case R.id.bAdd:
+                    add(number1, number2);
+                    break;
+                case R.id.bSub:
+                    subs(number1, number2);
+                    break;
+                case R.id.bMultiply:
+                    multiply(number1, number2);
+                    break;
+                case R.id.bDivide:
+                    divide(number1, number2);
+                    break;
+            }
+        }
+    }
+
+    private void add(int number1, int number2) {
+        int result = number1 + number2;
+        tvResult.setText(String.valueOf(result));
+    }
+
+    private void subs(int number1, int number2) {
+        int result = number1 - number2;
+        tvResult.setText(String.valueOf(result));
+    }
+
+    private void multiply(int number1, int number2) {
+        int result = number1 * number2;
+        tvResult.setText(String.valueOf(result));
+    }
+
+    private void divide(int number1, int number2) {
+        if(number2 == 0) {
+            Toast.makeText(this, "Nie dziel przez 0!", Toast.LENGTH_SHORT).show();
+        } else {
+            int result = number1 / number2;
+            tvResult.setText(String.valueOf(result));
+        }
+    }
+}
